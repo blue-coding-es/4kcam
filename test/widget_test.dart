61325507e33c03ebc565c4eb179edc290432f664
OXO <73500459+blue-coding-es@users.noreply.github.com>
2025-06-12 08:20:07 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:_4kcam/main.dart';
+import 'package:u_4kcam/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
