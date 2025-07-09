2c0e9370103f7f790584876c396bd98da4640e9e
OXO <73500459+blue-coding-es@users.noreply.github.com>
2025-07-09 12:24:04 +0200

    Update camera_provider.dart

--- lib/providers/camera_provider.dart
+++ lib/providers/camera_provider.dart
@@ -1,6 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../services/camera_control.dart';
+import '../services/camera_api.dart';
+import '../services/camera_api_v1.dart';
+import '../services/camera_api_v2.dart';
 
-final cameraControlProvider = Provider<CameraControl>((ref) {
-  return CameraControl();
+final cameraProvider = FutureProvider<CameraAPI>((ref) async {
+  // Detección automática
+  final isV1 = await _detectV1();
+  return isV1 ? CameraApiV1() : CameraApiV2();
 });
+
+Future<bool> _detectV1() async {
+  try {
+    final socket = await Socket.connect('192.168.1.254', 80, timeout: Duration(seconds: 2));
+    socket.destroy();
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
